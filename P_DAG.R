53a8b7e7ac82f3ca413874317f54560bf655c85d
AriG3 <arianna.gatta@mail.polimi.it>
2025-11-17 12:55:57 +0100

    P_DAG aggiornato

--- P_DAG.R
+++ P_DAG.R
@@ -9,8 +9,11 @@ P_DAG <- function(X, DAG, a, U) {
   pa <- parents(DAG,j[i])
   fa <- c(pa,j[i])  
   
-  prod_pa <- prod(P_XA(X,a,U,pa))
-  prod_fa <- prod(P_XA(X,a,U,fa))
+  u <- 1:length(pa)
+  v <- 1:length(fa)
+  
+  prod_pa <- prod(P_XA(X,a,U,pa[u]))
+  prod_fa <- prod(P_XA(X,a,U,fa[v]))
   
   P_DAG = prod_fa/prod_p12a
   return(P_DAG)
