fc51055822d66060fa081670dd1292eff96e050d
AriG3 <arianna.gatta@mail.polimi.it>
2026-02-05 20:59:44 +0100

    variable selection + skeleton

--- test/variable_selection_varbvs+horseshoe.R
+++ test/variable_selection_varbvs+horseshoe.R
@@ -2,6 +2,10 @@ library(hdi)
 library(varbvs)
 library(rstanarm)
 
+library(pcalg)
+library(Rgraphviz)
+source("C:/Users/arian/Documents/GitHub/BSProject2526/funzioni R/personalized_my_skeleton_senza_stable_fast.R")
+
 data(riboflavin)
 
 X <- riboflavin$x   # matrice delle covariate
@@ -19,7 +23,7 @@ keep <- names(sort(pip, decreasing = TRUE))[1:300]
 X_red <- Xsc[, keep, drop=FALSE]
 df <- data.frame(y = ysc, X_red)
 
-# contiene campioni posteriori
+# campioni a posteriori
 fit_hs <- stan_glm(
   y ~ .,        # tutte le altre colonne del data.frame (colonne di X_red)
   data = df,
@@ -27,7 +31,7 @@ fit_hs <- stan_glm(
   chains = 4, iter = 2000, seed = 1  # 4 catene MCMC indipendenti, 2000 iterazioni per chain
 )
 
-post <- as.matrix(fit_hs)  # Righe = campioni MCMC (tutte le chain concatenate); Colonne = coefficienti dei predittori
+post <- as.matrix(fit_hs)  # Righe = campioni MCMC (tutte le chain concatenate); Colonne = coefficienti di regressione delle covariate
 
 B <- post[, !colnames(post) %in% c("(Intercept)", "sigma"), drop = FALSE]
 
@@ -42,6 +46,19 @@ eps <- 0.1  # in scala standardizzata: 0.1 è un effetto "piccolo ma reale"
 p_big <- colMeans(abs(B) > eps) # media per colonna dei TRUE/FALSE (1/0): circa probabilità a posteriori che beta sia > soglia
 
 # Selezione:
-selected <- names(sort(p_big, decreasing = TRUE))[1:50]
+q = 10
+selected <- names(sort(p_big, decreasing = TRUE))[1:q]
 X_red <- Xsc[, selected, drop = FALSE]  
+
+
+
+# stampa skltn_bayes
+alpha_bayes <- 10
+skltn.bayes <- my_skeleton(X = X_red, a = q+1, U = diag(1, q), 
+                           alpha = alpha_bayes, 
+                           BayesTest = BF_Gaussian,
+                           saveBF = FALSE, verbose = FALSE)
+plot(skltn.bayes@graph)
+
+
 
