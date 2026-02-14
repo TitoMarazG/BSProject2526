00237bdd3622c36d3bfc724508fb9071e7098b94
AriG3 <arianna.gatta@mail.polimi.it>
2026-02-14 23:01:10 +0100

    Sub_graph + cambio colore edges + MANCA la probabilità di edge inclusion
    ho cambiato la label dell'egde logRiboflavin in Riboflavin perché secondo me è più carina da vedere nel grafo, tanto non ci sono i valori numerici quindi non penso sia importante mettere nel grafico il fatto che sia in scala logaritmica, magari lo diciamo a parte

---
+++ test/sub_graph_1000.R
@@ -0,0 +1,88 @@
+library(pcalg)
+library(Rgraphviz)
+library(igraph)
+library(graph)
+
+res_bayes <- readRDS("C:/Users/arian/Documents/GitHub/BSProject2526/test/Riboflavin_Bayes_q1000.rds")
+
+skltn.bayes <- res_bayes@graph
+adj <- as(skltn.bayes, "matrix")
+
+target_node <- "logRiboflavin"
+
+if (target_node %in% rownames(adj)) {
+  neigh <- colnames(adj)[adj[target_node, ] == 1]
+} else { character(0) }
+
+if(length(neigh)>0){
+  pos_neigh = which(adj[target_node, ] == 1)
+  
+  # neighbors of neighbors
+  neigh2 <- unique(unlist(lapply(neigh, function(nm) {
+    colnames(adj)[adj[nm, ] == 1]
+  })))
+  
+  nodes_keep <- unique(c(target_node, neigh, neigh2))
+  adj_red = adj[nodes_keep, nodes_keep, drop = FALSE]
+  
+  # subgrafo
+  g_sub <- graph_from_adjacency_matrix(adj_red, mode = "undirected")
+  # converti in graphNEL per plot
+  g_sub_nel <- as_graphnel(g_sub)    
+  
+  
+  #### DA FARE ####
+  # P = matrice di posterior probability of edge inclusion
+  nodes_in_sub = nodes(g_sub_nel)
+  P <- matrix(runif(length(nodes_in_sub)^2), 
+              nrow = length(nodes_in_sub), 
+              ncol = length(nodes_in_sub),
+              dimnames = list(nodes_in_sub, nodes_in_sub))
+  
+  
+  # ------ PLOT ------
+  
+  # cambio la label del nodo logriboflavin in Riboflavin
+  lbls <- nodes(g_sub_nel)
+  lbls[lbls == "logRiboflavin"] <- "Riboflavin"
+  nAttrs <- list(label = setNames(lbls, nodes(g_sub_nel)))
+  
+  # # cambio colore edges in base posterior probability of edge inclusion
+  
+  edge_list <- edgeL(g_sub_nel)
+  edge_cols <- character()
+  
+  for (node_from in names(edge_list)) {
+    tos <- edge_list[[node_from]]$edges
+    if (length(tos) == 0) next
+    
+    node_tos <- nodes(g_sub_nel)[tos]
+    
+    for (node_to in node_tos) {
+      # Per grafi non orientati, processiamo l'arco solo una volta (ordine alfabetico)
+      if (node_from < node_to) {
+        # Recuperiamo la probabilità dalla matrice P
+        # Se P non è simmetrica, potresti voler usare max(P[from,to], P[to,from])
+        p_val <- P[node_from, node_to]
+        
+        # Gestione NA o valori fuori range
+        if (is.na(p_val)) p_val <- 0
+        
+        # Creazione colore: 1 = Nero (p=1), 0.9 = Grigio quasi bianco (p=0)
+        # alpha determina la trasparenza, o puoi usare gray()
+        col_hex <- gray(1 - p_val) 
+        
+        # La chiave DEVE essere "nodo1~nodo2"
+        edge_key <- paste0(node_from, "~", node_to)
+        edge_cols[edge_key] <- col_hex
+      }
+    }
+  }
+  
+  edgeAttrs <- list(color = edge_cols)
+ 
+  # # # ---- Plot finale "graphNEL style" ---- 
+  plot(g_sub_nel, nodeAttrs = nAttrs, edgeAttrs = edgeAttrs, attrs = list(edge = list(lwd = 2)))
+  #plot(g_sub_nel, nodeAttrs = nAttrs)
+}
+
