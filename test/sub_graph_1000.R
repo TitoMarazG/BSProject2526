ca677d38522064b340fbb0885685cdba08530d00
AriG3 <arianna.gatta@mail.polimi.it>
2026-02-15 16:03:04 +0100

    risolto problema con P matrice di posterior prob

--- test/sub_graph_1000.R
+++ test/sub_graph_1000.R
@@ -3,10 +3,15 @@ library(Rgraphviz)
 library(igraph)
 library(graph)
 
-res_bayes <- readRDS("C:/Users/arian/Documents/GitHub/BSProject2526/test/Riboflavin_Bayes_q1000.rds")
+res_bayes <- readRDS("C:/Users/arian/Documents/GitHub/BSProject2526/test/RDS/Riboflavin_Bayes_q1000.rds")
 
 skltn.bayes <- res_bayes@graph
 adj <- as(skltn.bayes, "matrix")
+
+BF_max = res_bayes@pMax
+all_node_names <- nodes(res_bayes@graph)
+rownames(BF_max) <- all_node_names
+colnames(BF_max) <- all_node_names
 
 target_node <- "logRiboflavin"
 
@@ -31,14 +36,14 @@ if(length(neigh)>0){
   g_sub_nel <- as_graphnel(g_sub)    
   
   
-  #### DA FARE ####
   # P = matrice di posterior probability of edge inclusion
   nodes_in_sub = nodes(g_sub_nel)
-  P <- matrix(runif(length(nodes_in_sub)^2), 
-              nrow = length(nodes_in_sub), 
-              ncol = length(nodes_in_sub),
-              dimnames = list(nodes_in_sub, nodes_in_sub))
-  
+  BF_sub <- BF_max[nodes_keep, nodes_keep, drop = FALSE]
+  # Rendiamo la matrice simmetrica (necessario per grafi non orientati)
+  BF_sub[is.na(BF_sub)] <- Inf # Se non c'è evidenza, BF è infinito (P -> 0)
+  BF_sub_sym <- pmin(BF_sub, t(BF_sub)) # Usiamo il minimo BF per l'inclusione più probabile
+  P <- 1 / (1 + BF_sub_sym) # ipotizzando prior probability of edge inclusion = 0.5
+  diag(P) <- NA
   
   # ------ PLOT ------
   
@@ -63,7 +68,8 @@ if(length(neigh)>0){
       if (node_from < node_to) {
         # Recuperiamo la probabilità dalla matrice P
         # Se P non è simmetrica, potresti voler usare max(P[from,to], P[to,from])
-        p_val <- P[node_from, node_to]
+        p_val <- (P[node_from, node_to] - 0.9)/(1-0.9)
+        p_val <- p_val^2 #per aumentare contrasto
         
         # Gestione NA o valori fuori range
         if (is.na(p_val)) p_val <- 0
